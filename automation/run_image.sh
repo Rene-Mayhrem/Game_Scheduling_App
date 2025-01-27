8531c03eb05d94fcda1ec909fc2916e0f9a5621f
mayhrem <renecruz.0711@gmail.com>
2025-01-26 20:37:27 -0600

    Adding scripts

--- automation/run_image.sh
+++ automation/run_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NFL_DIF="/home/mayhrem/devops-nba-challenge/Game_Scheduling_App/src"
+NFL_DIF="/home/mayhrem/Documents/devops-challenge/Game_Scheduling_App/src"
 
 docker build -t nfl-backend $NFL_DIF
 docker run -p 5000:5000 nfl-backend
